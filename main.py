47d9390b92cc429cfb8bfdf26827f702ce4e0492
Salih Emin Kara <slhemnkr@gmail.com>
2024-10-03 08:53:22 +0300

    main added

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Python Turtle")
+turtle_istance = turtle.Turtle()
+
+
+
+
+turtle.mainloop()
+
+
+
+
